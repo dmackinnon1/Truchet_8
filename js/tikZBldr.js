4991d6532f2d85c46ac3eeb67f6901c0394d127d
Dan MacKinnon <dmackinnon1@gmail.com>
2025-02-15 22:03:42 -0500

    semicircles

--- js/tikZBldr.js
+++ js/tikZBldr.js
@@ -54,7 +54,8 @@ class TikZBuilder {
 	}
 	drawTriangle(x1, y1, r=0,scale=1){
 		let topLeft = new TikZPoint(x1, y1,r);
-		this.components.push(new TikZRightTriangle(topLeft,r,scale));
+	   //this.components.push(new TikZRightTriangle(topLeft,r,scale));
+		this.components.push(new TiKZBlackSemiCircle(topLeft,r,scale));
 	}
 
 	drawDiagonal(x1, y1, r=0,scale=1){
@@ -97,6 +98,62 @@ constructor(topLeft, rotation=0, size=1,color="black"){
 	}
 
 }
+
+
+class TiKZBlackSemiCircle{
+
+constructor(topLeft, rotation=0, size=1,color="black"){
+		this.color = color;
+		this.size= size;
+		this.topLeft = topLeft;
+		this.rotation = rotation;
+		}
+	
+	build(){
+		let dash = "--";
+		let topLeft = this.topLeft;
+		
+		topLeft.setLabel("A");
+		let topRight = new TikZPoint(topLeft.x + this.size, topLeft.y);
+		topRight.setLabel("B");
+		
+		let bottomRight = new TikZPoint(topLeft.x + this.size, topLeft.y - this.size);
+		bottomRight.setLabel("C");
+		
+		let bottomLeft = new TikZPoint(topLeft.x, topLeft.y - this.size);
+		bottomLeft.setLabel("D");
+		
+		let s = "";
+		s += topLeft.coordinateDef() + "\n";
+		s += topRight.coordinateDef() + "\n";
+		s += bottomRight.coordinateDef() + "\n";
+		s += bottomLeft.coordinateDef() + "\n";
+
+		let radius = this.size;
+
+		s+= "\\draw (A)--(B)--(C)--(D)--cycle;\n";
+		if (this.rotation==0){
+			//s = "\\draw[opacity=1,fill=black]" +  topLeft.build() + dash + topRight.build() + dash + bottomRight.build()+"; \n";
+			s += "\\draw[opacity=1,fill=black]" +  topRight.coordinate()+" arc[start angle=0, end angle=90, radius=" + radius +"]; \n";
+		}
+		if (this.rotation==1){
+			//s = "\\draw[opacity=1,fill=black]" +  topRight.build() + dash + bottomRight.build() + dash + bottomLeft.build()+"; \n";
+			s += "\\draw[opacity=1,fill=black]" +  bottomRight.coordinate()+" arc[start angle=0, end angle=90, radius=" + radius +"]; \n";
+		}
+		if (this.rotation==2){
+			//s = "\\draw[opacity=1,fill=black]" +  bottomRight.build() + dash + bottomLeft.build()+ dash + topLeft.build() +"; \n";
+			s += "\\draw[opacity=1,fill=black]" +  bottomLeft.coordinate()+" arc[start angle=0, end angle=90, radius=" + radius +"]; \n";
+		}
+		if (this.rotation==3){
+			//s = "\\draw[opacity=1,fill=black]" +  bottomLeft.build()+ dash + topLeft.build() + dash + topRight.build() +"; \n";
+			s += "\\draw[opacity=1,fill=black]" +  topLeft.coordinate()+" arc[start angle=0, end angle=90, radius=" + radius +"]; \n";
+		}
+		return s;
+
+	}
+
+}
+
 
 class TikZRightDiagonal{
 
@@ -177,10 +234,24 @@ class TikZPoint {
 	constructor(x,y){
 		this.x = x;
 		this.y = y;
+		this.label = null;
+	}
+
+	setLabel(l){
+		this.label = l;
+	}
+
+	coordinateDef(){
+		return "\\coordinate ("+this.label +") at " + this.build() +";";
+	}
+
+	coordinate(){
+		return "(" + this.label + ")";
 	}
 
 	build(){
 		return "(" + this.x + "," + this.y + ")";
+
 	}
 
 }
